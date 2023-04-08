2a7629368f66f3a96783a1fbc003b80152ade842
Bino <you@example.com>
2023-04-08 14:15:05 -0400

    add get geo location

--- src/home/trending/index.js
+++ src/home/trending/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { findPostsThunk } from "../../services/posts-thunks";
 
@@ -6,9 +6,30 @@ const TrendingList = () => {
     const { posts, loading, error } = useSelector(state => state.postsData);
     const dispatch = useDispatch();
 
+    const [location, setLocation] = useState({});
+
+    const getUserLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setLocation({ latitude, longitude });
+                },
+                (error) => {
+                    console.error("Error retrieving user location:", error);
+                }
+            );
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    };
+
     useEffect(() => {
         // Dispatch the findPostsThunk to fetch posts when the component mounts
         dispatch(findPostsThunk());
+
+        // Get user location when the component mounts
+        getUserLocation();
     }, [dispatch]);
 
     if (loading) {
@@ -21,7 +42,12 @@ const TrendingList = () => {
 
     return (
         <div>
-            <h1>Post List</h1>
+            <h1>
+                Trending at{" "}
+                {location.latitude && location.longitude
+                    ? `(${location.latitude.toFixed(2)}, ${location.longitude.toFixed(2)})`
+                    : "your location"}
+            </h1>
             <ul>
                 {posts.map(post => (
                     <li key={post.id}>{post.username}</li>
