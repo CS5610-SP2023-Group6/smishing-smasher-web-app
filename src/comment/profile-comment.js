ed248cb3fa14ff57ea82662384701eb54756bd4e
b1n024 <101355150+b1n024@users.noreply.github.com>
2023-04-23 15:58:50 -0400

    profile cmt done

--- src/comment/profile-comment.js
+++ src/comment/profile-comment.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../login/auth-context";
-import { isLoggedInService } from "../services/auth/is-logged-in";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../login/auth-context";
+import {isLoggedInService} from "../services/auth/is-logged-in";
 import {createComment, findCommentById, findCommentsByAuthorId, updateComment} from "../services/comment-service"; // Import 'findCommentsByUserId'
-import { fetchCurrentUserProfile } from "../services/cur-user-service";
-import { findUserById } from "../services/user-service";
+import {fetchCurrentUserProfile} from "../services/cur-user-service";
+import {findUserById} from "../services/user-service";
 import {updatePost} from "../services/posts/posts-service";
+import Filter from "../profile/filter";
 
-const CommentItem = ({ uid }) => {
+const CommentItem = ({uid}) => {
 
     const [comments, setComments] = useState([]); // Create a state for comments
     const navigate = useNavigate();
@@ -16,8 +17,24 @@ const CommentItem = ({ uid }) => {
     const [user, setUser] = useState(null);
     const [commentAuthor, setCommentAuthor] = useState(null);
     const [avatar, setAvatar] = useState("");
+    const [filteredData, setFilteredData] = useState([]);
+    const filterData = (months) => {
+        const now = new Date();
+        const filtered = comments.filter((item) => {
+            const itemDate = new Date(item.createdAt); // Use 'item.time' instead of 'item.datePosted'
+            const timeDifference = now - itemDate;
+            const monthsDifference = timeDifference / 1000 / 60 / 60 / 24 / 30;
+            console.log(months);
+            console.log(monthsDifference);
+            return monthsDifference <= months;
+        });
 
+        setFilteredData(filtered);
+    };
 
+    const handleFilter = (months) => {
+        filterData(months);
+    };
 
     console.log("uid, profile-comment", uid);
     useEffect(() => {
@@ -25,7 +42,7 @@ const CommentItem = ({ uid }) => {
             const currentUserProfile = await fetchCurrentUserProfile();
             console.log("uid, profile-comment", uid);
 
-            const api = axios.create({ withCredentials: true });
+            const api = axios.create({withCredentials: true});
             const res = await api.get(`http://localhost:4000/api/users/id/${uid}`);
 
             if (res.data.profilePicture !== undefined) {
@@ -51,25 +68,38 @@ const CommentItem = ({ uid }) => {
             {comments.length === 0 ? (
                 <div>No comments found.</div>
             ) : (
-                comments.map((comment) => (
-                    <div key={comment._id}>
-                        <li className="list-group-item">
-                            <div className="row">
-                                <div className="col-2">
-                                    <img
-                                        className="float-end rounded-pill"
-                                        height="60px"
-                                        src={avatar}
-                                    />
-                                </div>
-                                <div className="col-10">
-                                    <div className="mb-1">{comment.text}</div>
-
-                                </div>
-                            </div>
-                        </li>
+                <div>
+                    <div className="col-5">
+                        <Filter onFilter={handleFilter} />
                     </div>
-                ))
+                    {comments.map((comment) => {
+                        const time = new Date(comment.createdAt);
+                        return (
+                            <div key={comment._id}>
+                                <li className="list-group-item">
+                                    <div className="row">
+                                        <div className="col-2">
+                                            <img
+                                                className="float-end rounded-pill"
+                                                height="60px"
+                                                src={avatar}
+                                            />
+                                        </div>
+                                        <div className="col-10">
+                                            <div className="mb-1 text-secondary">
+                                                {time.getFullYear()}-{time.getMonth()}-{time.getDate()}&nbsp;
+                                                {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}&nbsp;
+
+                                            </div>
+                                            {comment.text}
+                                        </div>
+                                        <hr />
+                                    </div>
+                                </li>
+                            </div>
+                        );
+                    })}
+                </div>
             )}
         </div>
     );
